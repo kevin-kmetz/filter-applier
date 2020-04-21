0f43bd9598137205ce17f5d2829a0323afa95df8
Kevin_Kmetz <kev.kmetz@gmail.com>
2020-04-21 09:46:43 -0400

    Starting adding abstract methods to ImageFilter.

--- FilterApplier/ImageFilter.java
+++ FilterApplier/ImageFilter.java
@@ -1,5 +1,16 @@
 package FilterApplier;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+
 abstract class ImageFilter {
 	
+	abstract BufferedImage apply(BufferedImage sourceImage);
+
+	abstract BufferedImage apply(String sourceImageFileName);
+
+	abstract boolean apply(BufferedImage sourceImage, String ouputFileName);
+
+	abstract boolean apply(String sourceImageFileName, String outputFileName);
 }
